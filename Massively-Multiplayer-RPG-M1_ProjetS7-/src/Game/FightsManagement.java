e65df0a6c1ee5bf70dfb624d00f5580215015a98
Bloch Tom <tom.bloch@univ-tlse3.fr>
2018-12-29 19:02:58 +0100

    Version fight test

--- Massively-Multiplayer-RPG-M1_ProjetS7-/src/Game/FightsManagement.java
+++ Massively-Multiplayer-RPG-M1_ProjetS7-/src/Game/FightsManagement.java
@@ -7,13 +7,33 @@ public class FightsManagement extends Thread {
     private HashSet<Fight> fights;
 
     public FightsManagement() {
-        this.fights = new HashSet<Fight>();
+        fights = new HashSet<Fight>();
     }
 
-    public void addFight(Fight fight){
-        this.fights.add(fight);
+    public synchronized void addFight(Fight fight){
+        fights.add(fight);
+        notify();
+    }
+
+    public void waitFight() throws InterruptedException {
+        while(fights.size()==0){
+            wait();
+        }
+    }
+
+    public void fight(){
+        //TODO : Version test
+        for (Fight fight : fights ) {
+            fight.getAttacked().setLife(2);
+        }
     }
 
     public void run(){
+        try {
+            waitFight();
+        } catch (InterruptedException e) {
+            System.out.println("is a test");
+        }
+        fight();
     }
 }
--- Massively-Multiplayer-RPG-M1_ProjetS7-/src/Labyrinth/LabyrinthImpl.java
+++ Massively-Multiplayer-RPG-M1_ProjetS7-/src/Labyrinth/LabyrinthImpl.java
@@ -6,4 +6,24 @@ import static java.lang.Thread.sleep;
 
 public class LabyrinthImpl {
 
+    public static void main (String[] args) {
+
+        // TODO : Version test
+
+        System.out.println("c'est parti !");
+        Player player=new Player("Boris", 10,1,0,1,null);
+        Monster monster = new Monster("chupacabra",10,1,0,1);
+        FightsManagement fm = new FightsManagement();
+        Fight fight= new Fight(player,monster);
+        fm.start();
+        fm.addFight(fight);
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(monster.getLife()+ "    and     "+player.getLife());
+    }
+
+
 }
