705fee3f583d9972e39672aa8651862817672a37
Bloch Tom <tom.bloch@univ-tlse3.fr>
2018-12-30 19:55:46 +0100

    context

--- Massively-Multiplayer-RPG-M1_ProjetS7-/src/Game/Context.java
+++ Massively-Multiplayer-RPG-M1_ProjetS7-/src/Game/Context.java
@@ -1,7 +1,17 @@
 package Game;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.Vector;
 
 public class Context implements Serializable {
+
+    private List<Monster> monsters;
+    private ArrayList<Player> players;
+
+    public void t(){
+    }
 
 }
