aa1a78e1c0c443f25792c8fe42809b1a3790b34b
James Bosse <james.bosse.jack@gmail.com>
2019-01-02 20:46:10 +0100

    Hall

--- Massively-Multiplayer-RPG-M1_ProjetS7-/src/Game/Hall.java
+++ Massively-Multiplayer-RPG-M1_ProjetS7-/src/Game/Hall.java
@@ -1,11 +1,20 @@
 package Game;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class Hall {
     private String name;
     private static Map<Hall,Door>  Hall_liste = new HashMap<Hall,Door>();
+
+
+    private List<Participant> list = new LinkedList<>();
+
+
+    public List<Participant> getList() {
+        return list ;
+    }
+
     public Hall{
 
         this.name=name;
@@ -21,26 +30,26 @@ public class Hall {
 
     }
 
-    public void addDoor(Door door, Pole pole){
-
+    public void addDoor(Hall hall,Door door, Pole pole){
+        Hall_liste.put(hall.name,)
     }
     public void addMonster(Monster monster){
-        Hall_liste.
+        list.add(monster);
     }
 
     public void addPlayer(Player player){
-
+        list.add(player);
 
     }
 
     public void removeMonster(Monster monster){
-
+        Hall_liste.remove(monster);
 
     }
 
     public  void removePlayer(Player player){
 
-
+        Hall_liste.remove(player);
     }
 
     public void hitpoints(Participant participant, int hitpoint){
