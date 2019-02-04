b790da65f3a99111837b62b0f0ce1e42b848678d
DjebbarNadji <djebbar.nadji@gmail.com>
2019-02-05 00:14:10 +0100

    Labyrinth(Impl) Act1

--- Massively-Multiplayer-RPG-M1_ProjetS7-/src/Labyrinth/LabyrinthImpl.java
+++ Massively-Multiplayer-RPG-M1_ProjetS7-/src/Labyrinth/LabyrinthImpl.java
@@ -4,7 +4,24 @@ import Game.*;
 
 import static java.lang.Thread.sleep;
 
-public class LabyrinthImpl {
+import java.rmi.RemoteException;
+
+import Client.Session;
+
+public class LabyrinthImpl implements Labyrinth{
+	
+	public  Context login(Session s) throws RemoteException {
+		return null;
+	}
+	public Context changeHall(String Hall, String player, Pole direction) throws RemoteException {
+		return null;
+	}
+	public void newFight(Participant forward, Participant attacked) throws RemoteException {
+	}
+	public void runnaway(Participant forward, Participant attacked) throws RemoteException {
+	}
+	public void logOut(String player) throws RemoteException {
+	}
 
 
 }
