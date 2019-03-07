28b8728ae23b28c7f50a9300faf15f9e564ba0f5
Unknown <james.bosse.jack@gmail.com>
2019-03-07 18:29:44 +0100

    PLAYER

--- Massively-Multiplayer-RPG-M1_ProjetS7-/src/Labyrinth/Labyrinth.java
+++ Massively-Multiplayer-RPG-M1_ProjetS7-/src/Labyrinth/Labyrinth.java
@@ -7,12 +7,12 @@ import java.util.HashMap;
 import Game.*;
 import Client.*;
 public interface Labyrinth extends Remote {
-	
+
 	public void login(Session s, Client proxy) throws RemoteException;
 	public void changeHall(String Hall, String player, Pole direction) throws RemoteException;
-	public void newFight(String forward, String attacked) throws RemoteException;
+	public void newFight(String idHall, String forward, String attacked) throws RemoteException;
 	public void runnaway(String Hall, String forward, String runner) throws RemoteException;
-	public void logOut(String Hall, String player) throws RemoteException;
+	public void logout(String Hall, String player) throws RemoteException;
 	public void setReponsabiities(HashMap<Labyrinth,ArrayList<String>> resp) throws RemoteException;
 
 }
